c4981bc38aa59c35d97257cc993ae0796b61f642
Faith Janicek <faithe1937@gmail.com>
2019-10-30 13:49:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
 class Dog
- 
-  def initialize(name,breed)
-    @name = name
-    @breed = breed 
+    
+  def name
+    @name
   end
- 
- 
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  
 end
-
-
